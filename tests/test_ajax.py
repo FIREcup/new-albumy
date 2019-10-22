fcce20b31c5e1d927a2cdba30a42a5ef15554788
FIREcup <1039314612@qq.com>
2019-10-23 00:20:43 +0800

    test

--- tests/test_ajax.py
+++ tests/test_ajax.py
@@ -8,12 +8,12 @@ class AjaxTextCase(BaseTestCase):
     def test_notification_count(self):
         response = self.client.get(url_for('ajax.notification_count'))
         data = response.get_json()
-        slef.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 403)
         self.assertEqual(data['message'], 'Login required')
 
         self.login()
         response = self.client.get(url_for('ajax.notification_count'))
-        slef.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_get_profile(self):
         response = self.client.get(url_for('ajax.get_profile', user_id=1))
@@ -22,10 +22,10 @@ class AjaxTextCase(BaseTestCase):
         self.assertIn('Admin', data)
 
     def test_followers_count(self):
-        response = slef.client.get(url_for('ajax.followers_count', user_id=1))
+        response = self.client.get(url_for('ajax.followers_count', user_id=1))
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
-        slef.assertEqual(data['count'], 0)
+        self.assertEqual(data['count'], 0)
 
         user = User.query.get(2)
         user.follow(User.query.get(1))
@@ -36,10 +36,10 @@ class AjaxTextCase(BaseTestCase):
         self.assertEqual(data['count'], 1)
 
     def test_collectors_count(self):
-        response = slef.client.get(url_for('ajax.collectors_count', photo_id=1))
+        response = self.client.get(url_for('ajax.collectors_count', photo_id=1))
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
-        slef.assertEqual(data['count'], 0)
+        self.assertEqual(data['count'], 0)
 
         user = User.query.get(1)
         user.collect(Photo.query.get(1))
@@ -50,10 +50,10 @@ class AjaxTextCase(BaseTestCase):
         self.assertEqual(data['count'], 1)
 
     def test_collect(self):
-        response = slef.client.post(url_for('ajax.collect', photo_id=1))
+        response = self.client.post(url_for('ajax.collect', photo_id=1))
         data = response.get_json()
         self.assertEqual(response.status_code, 403)
-        slef.assertEqual(data['message'], 'Login Required')
+        self.assertEqual(data['message'], 'Login Required')
 
         self.login(email='unconfirmed@helloflask.com', password='123')
         response = self.client.post(url_for('ajax.collect', photo_id=1))
@@ -71,4 +71,73 @@ class AjaxTextCase(BaseTestCase):
         response = self.client.post(url_for('ajax.collect', photo_id=1))
         data = response.get_json()
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(data['message'], 'Photo collected.')
+        self.assertEqual(data['message'], 'Already collected.')
+
+    def test_uncollect(self):
+        response = self.client.post(url_for('ajax.uncollect', photo_id=1))
+        data = response.get_json()
+        self.assertEqual(response.staus_code, 403)
+        self.assertEqual(data['message'], 'Login required')
+
+        self.login()
+        response = self.client.post(url_for('ajax.uncollect', photo_id=1))
+        data = response.get_json()
+        self.assertEqual(response.staus_code, 400)
+        self.assertEqual(data['message'], 'Not collect yet.')
+
+        user = User.query.get(2)
+        user.collect(Photo.query.get(1))
+
+        response = self.client.post(url_for('ajax.uncollect', photo_id=1))
+        data = response.get_json()
+        self.assertEqual(response.staus_code, 200)
+        self.assertEqual(data['message'], 'Collect canceled.')
+
+    def test_follow(self):
+        response = self.client.post(url_for('ajax.follow', username='admin'))
+        data = response.get_json()
+        self.assertEqual(response.staus_code, 403)
+        self.assertEqual(data['message'], 'Login  required..')
+
+        self.login(email='unconfirmed@helloflask.com', password='123')
+        response = self.client.post(url_for('ajax.follow', username='admin'))
+        data = response.get_json()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Confirm account required.')
+        self.logout()
+
+        self.login()
+        response = self.client.post(url_for('ajax.follow', username='normal'))
+        data = response.get_json()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Already followed.')
+
+        response = self.client.post(url_for('ajax.follow', username='admin'))
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['message'], 'User followed.')
+
+    def test_unfollow(self):
+        response = self.client.post(url_for('ajax.unfollow', username='admin'))
+        data = response.get_json()
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(data['message'], 'Login required.')
+
+        self.login()
+        response = self.client.post(url_for('ajax.unfollow', username='admin'))
+        data = response.get_json()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['message'], 'Not follow yet.')
+
+        user = User.query.get(2)
+        user.follow(User.query.get(1))
+
+        response = self.client.post(url_for('ajax.unfollow', username='admin'))
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['message'], 'Follow canceled.')
+
+
+
+
+
