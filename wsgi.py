a3f651974d0b429925632c732d6d587122adf8d2
yangtao <yangtao@linux.com>
2019-12-18 12:20:41 +0800

    modify wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,5 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from albumy import create_app("production") as app
+from albumy import create_app
+app = create_app("production")
